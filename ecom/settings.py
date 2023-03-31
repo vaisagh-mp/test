f7f2f956282c5f9bf8fa107394e63ed715667077
vaisagh-mp <vaisaghmp@gmail.com>
2023-03-31 14:37:51 +0530

    db

--- ecom/settings.py
+++ ecom/settings.py
@@ -79,10 +79,21 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecom',
+        'USER': 'ecom',
+        'PASSWORD': 'ecom',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
